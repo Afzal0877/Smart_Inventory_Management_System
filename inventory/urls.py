4a7e0dcb3cc44f7a0043e6aca205e0af04c5d272
Afzal Khan <afzalkhanznz@gmail.com>
2026-08-04 17:19:33 +0530

    Completed Smart Inventory Management System

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+
     path('categories/', views.category_list, name='category_list'),
     path('categories/add/', views.add_category, name='add_category'),
+
     path("products/", views.product_list, name="product_list"),
-path("products/add/", views.add_product, name="add_product"),
-path("suppliers/", views.supplier_list, name="supplier_list"),
-path("suppliers/add/", views.add_supplier, name="add_supplier"),
+    path("products/add/", views.add_product, name="add_product"),
+    path("products/edit/<int:pk>/", views.edit_product, name="edit_product"),
+
+    path("suppliers/", views.supplier_list, name="supplier_list"),
+    path("suppliers/add/", views.add_supplier, name="add_supplier"),
+    path("products/delete/<int:pk>/", views.delete_product, name="delete_product"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render, redirect
+from django.db.models import F
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category, Product, Supplier
 from .forms import ProductForm, SupplierForm
 
 
 def dashboard(request):
-    return render(request, 'inventory/dashboard.html')
+
+    total_products = Product.objects.count()
+    total_categories = Category.objects.count()
+    total_suppliers = Supplier.objects.count()
+
+    low_stock_products = Product.objects.filter(
+        quantity__lt=F("minimum_stock")
+    )
+
+    low_stock = low_stock_products.count()
+
+    return render(request, "inventory/dashboard.html", {
+        "total_products": total_products,
+        "total_categories": total_categories,
+        "total_suppliers": total_suppliers,
+        "low_stock": low_stock,
+        "low_stock_products": low_stock_products,
+    })
+    
+
 
 
 
@@ -29,7 +49,13 @@ def add_category(request):
 
     return render(request, 'inventory/category_form.html')
 def product_list(request):
+
     products = Product.objects.all()
+
+    search = request.GET.get("search")
+
+    if search:
+        products = products.filter(name__icontains=search)
 
     return render(request, "inventory/product_list.html", {
         "products": products
@@ -71,3 +97,27 @@ def add_supplier(request):
     return render(request, "inventory/supplier_form.html", {
         "form": form
     })
+def edit_product(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+
+        if form.is_valid():
+            form.save()
+            return redirect("product_list")
+
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, "inventory/product_form.html", {
+        "form": form
+    })
+def delete_product(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+
+    product.delete()
+
+    return redirect("product_list")
